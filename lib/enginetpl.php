1a9681e9c177aa95259ff61854a7a7f1bf853755
m0os3s547 <128752319+m0os3s547@users.noreply.github.com>
2023-11-17 19:20:17 -0300

    Update enginetpl.php

--- lib/enginetpl.php
+++ lib/enginetpl.php
@@ -1,51 +1,48 @@
-<?php 
+<?php
+class EngineTpl
+{
+    public $url_tpl;
+    public $tpl;
 
+    function __construct($url_tpl)
+    {
+        $this->url_tpl = $url_tpl;
 
-	/**
-	 * 
-	 */
-	class EngineTpl
-	{
-		
-		public $url_tpl;
-		public $tpl;
+        $this->tpl = file_get_contents($url_tpl);
 
-		function __construct($url_tpl)
-		{
+        if ($this->testVar("URL_APP")) {
+            $this->assignVar("URL_APP", URL_APP);
+        }
 
-			$this->url_tpl = $url_tpl;
+        if ($this->testVar("CACHE")) {
+            if (CACHE == MODO_DEBUG) {
+                $this->assignVar("CACHE", "?rand=" . date("YmdHis"));
+            } else {
+                $this->assignVar("CACHE", "");
+            }
+        }
+    }
 
-			$this->tpl = file_get_contents($url_tpl);
+    private function testVar($var_tpl)
+    {
+        return strpos($this->tpl, "{{{$var_tpl}}}");
+    }
 
-			if($this->testVar("URL_APP"))
-				$this->assignVar("URL_APP", URL_APP);
+    public function assignVar($var_tpl, $value)
+    {
+        $pos = $this->testVar($var_tpl);
 
-			if($this->testVar("CACHE"))
-				if(CACHE==MODO_DEBUG)
-					$this->assignVar("CACHE", "?rand=".date("YmdHis"));
-				else
-					$this->assignVar("CACHE", "");
-		}
+        if ($pos !== false) {
+            $this->tpl = substr_replace($this->tpl, $value, $pos, strlen("{{{$var_tpl}}}"));
+        } else {
+            // Manejar el caso en el que no se encontró la variable
+            echo "<b>error tpl:</b> No se encontró la variable <u>$var_tpl</u>";
+        }
+    }
 
-		private function testVar($var_tpl){
-			return strpos($this->tpl, "{{{$var_tpl}}}");		
-		}
-
-
-		public function assignVar($var_tpl, $value){
-
-			if(!$this->testVar($var_tpl)){
-				echo "<b>error tpl:</b> No se encontro la variable <u>$var_tpl</u>";
-			}
-
-			$this->tpl=str_replace("{{{$var_tpl}}}", $value, $this->tpl);
-		}
-
-
-		public function printToScreen(){
-			echo $this->tpl;
-		}
-	}
-
-
- ?>
+    public function printToScreen()
+    {
+        echo $this->tpl;
+    }
+}
+?>
