f744e1b765f8e4bdc2e93d672e1fb5cab8dadf9d
m0os3s547 <128752319+m0os3s547@users.noreply.github.com>
2023-11-17 19:23:43 -0300

    Update panelController.php

--- Controllers/panelController.php
+++ Controllers/panelController.php
@@ -1,16 +1,7 @@
-<?php
-// Cargar el modelo necesario si es aplicable
-include '../Models/estacionModel.php'; // Asegúrate de que el nombre del modelo sea correcto
+<?php 
 
-// Lógica para obtener los datos de las estaciones desde el modelo
-$estaciones = obtenerEstaciones(); // Define esta función en tu modelo
+	$tpl = new EngineTpl('./Views/panelView.html');
 
-// Cargar la plantilla del motor de plantillas
-$template = new EngineTpl('../Views/panelView.html'); // Ruta correcta a tu plantilla
+	$tpl->printToScreen();
 
-// Asignar variables a la plantilla si es necesario
-$template->assignVar('estaciones', $estaciones);
-
-// Imprimir la plantilla en pantalla
-$template->printToScreen();
-?>
+ ?>
