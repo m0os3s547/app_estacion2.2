4d16ec49f82e1e34463680a7f5b35cd5ef9594bd
m0os3s547 <128752319+m0os3s547@users.noreply.github.com>
2023-11-17 19:19:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,38 @@
 <?php
-// Redirigir al usuario a landingView.html solo si no hay un error 404
-if ($_SERVER['REDIRECT_STATUS'] !== '404') {
-    header("Location: Views/landingView.html");
-    exit();
-}
-require_once('env.php');
 session_start();
-
-// Incluye el motor de plantillas
-include './lib/enginetpl.php';
-
-// Función para redirigir a una página de error 404
-function pageNotFound() {
-    header('HTTP/1.0 404 Not Found');
-    include './Views/error404View.html';
-    exit();
+include('env.php');
+if (!isset($_SESSION[APP_NAME])) {
+    $_SESSION[APP_NAME] = array();
 }
 
-// Ruta amigable: si la URL es "/panel", se redirige a "panel"
-$uri = $_SERVER['REQUEST_URI'];
-$baseURL = str_replace('index.php', '', $_SERVER['SCRIPT_NAME']);
-$section = trim(str_replace($baseURL, '', $uri), '/');
+include('./lib/enginetpl.php');
+$seccion = isset($_GET["seccion"]) ? $_GET["seccion"] : "";
 
-// Lógica de autenticación
-if (isset($_SESSION[APP_NAME]["user_name"])) {
-    // Usuario logueado
-    if (empty($section) || $section === "landingView") {
-        $section = "panelView";
+$_ROUTE = explode("/", $seccion);
+// router
+
+if ($_ROUTE[0] != "") {
+    $section = $_ROUTE[0];
+
+    $controllerFileName = "{$section}Controller.php";
+    $controllerFilePath = "./Controllers/{$controllerFileName}";
+
+    if (!file_exists($controllerFilePath)) {
+        $section = "error404";
+        $controllerFileName = "{$section}Controller.php";
+        $controllerFilePath = "./Controllers/{$controllerFileName}";
     }
 } else {
-    // Usuario no logueado
-    if (empty($section) || $section === "panelView") {
-        $section = "landingni";
-    }
+    $section = "landing";
+    $controllerFileName = "{$section}Controller.php";
+    $controllerFilePath = "./Controllers/{$controllerFileName}";
 }
 
-// Ruta de controladores
-$controller = "./Controllers/{$section}Controller.php";
 
-// Verificar si el controlador existe
-if (file_exists($controller)) {
-    // Cargar el controlador correspondiente
-    include $controller;
-} else {
-    // Página no encontrada
-    pageNotFound();
-}
+// ...
+
+
+
+include $controllerFilePath;
+
 ?>
