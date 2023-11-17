ea2e461760f1ecb729b174e704a5c94ea6452d63
m0os3s547 <128752319+m0os3s547@users.noreply.github.com>
2023-11-17 19:24:04 -0300

    Update landingController.php

--- Controllers/landingController.php
+++ Controllers/landingController.php
@@ -1,22 +1,9 @@
-<?php
-require_once('../lib/enginetpl.php'); // Asegúrate de incluir el archivo EngineTpl actualizado
+<?php 
 
-class LandingController {
-    public function index() {
-        // Cargar el motor de plantillas
-        $template = new EngineTpl('../Views/landingView.html'); // Especifica la ubicación de la plantilla
 
-        // Datos para pasar a la vista
-        $data = array(
-            'title' => 'Página de Inicio',
-            'content' => 'Bienvenido a la página de inicio. Aquí puedes agregar contenido adicional.',
-        );
+    $tpl = new EngineTpl('./Views/landingView.html');
 
-        // Asignar variables a la plantilla
-        $template->assignVar('title', $data['title']);
-        $template->assignVar('content', $data['content']);
+    $tpl->printToScreen();
 
-        // Imprimir la plantilla en la pantalla
-        $template->printToScreen();
-    }
-}
+
+ ?>
